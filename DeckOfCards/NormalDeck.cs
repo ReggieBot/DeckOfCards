31b66b05db5ddc0981e0cd618af633af43fb7429
ReggieBot <reggiestephenbrown@gmail.com>
2025-12-09 12:20:28 -0500

    Completed NormalDeck.cs. Extremely simple class

--- DeckOfCards/NormalDeck.cs
+++ DeckOfCards/NormalDeck.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿// Deck of Cards Project: COSC2100
+// Reggie Brown
+// December 9, 2025
+// Professor Fabian
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// standard 52-card deck
+// inherits from Deck class
+// Reference: https://www.w3schools.com/cs/cs_inheritance.php
 namespace DeckOfCards
 {
-    internal class NormalDeck
+    public class NormalDeck : Deck
     {
+        public NormalDeck()
+        {
+            CreateDeck();
+        }
     }
 }
