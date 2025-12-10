b3f3077575f9106eb910936c0e43509ebf18527c
ReggieBot <reggiestephenbrown@gmail.com>
2025-12-10 10:04:31 -0500

    CustomDeck class implemented. Getting started on program.cs now

---
+++ DeckOfCards/CustomDeck.cs
@@ -0,0 +1,33 @@
+﻿// Deck of Cards Project: COSC2100
+// Reggie Brown
+// December 10, 2025
+// Professor Fabian
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Custom deck class to allow users to start with existing cards
+// and accept additional custom cards
+// inherits from Deck class
+namespace DeckOfCards
+{
+    public class CustomDeck : Deck
+    {
+        // constructor for empty custom deck
+        public CustomDeck()
+        {
+        }
+
+        public CustomDeck(List<Card> initialCards)
+        {
+            for (int i = 0; i < initialCards.Count; i++)
+            {
+                cards.Add(initialCards[i]);
+            }
+        }
+    }
+}
